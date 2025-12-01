30daf44a7b83ae18b75ec92feecc81313ac8d5e7
pickanapple <thasselgreen@gmail.com>
2025-12-01 12:50:35 +0100

    solved d1p1

---
+++ day1/part1.py
@@ -0,0 +1,24 @@
+dial = 50
+count = 0
+
+def rotate(direction, number, dial): 
+    if direction == "L": 
+        return (dial - number) % 100
+        
+    else: 
+        return (dial + number) % 100
+    
+# with open("day1/testInput.txt") as f: 
+with open("day1/input.txt") as f: 
+    instructions = f.readlines()
+    instructionsDecoded = []
+
+    for i in instructions: 
+        instructionsDecoded.append((i[0], int(i[1:].replace("\n", ""))))
+
+for i in instructionsDecoded: 
+    dial = rotate(i[0], i[1], dial)
+    print(dial)
+    if dial == 0: count += 1
+
+print(count)

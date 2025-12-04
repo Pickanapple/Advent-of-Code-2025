a6af6b9be1111fa9e16839c57ba753a57b4918dd
pickanapple <thasselgreen@gmail.com>
2025-12-04 11:59:12 +0100

    d4p2

--- day4/part2.py
+++ day4/part2.py
@@ -1,3 +1,34 @@
+def findValid(grid): 
+    total = 0
+    loopRemoved = 1
+
+    while loopRemoved > 0: 
+        loopRemoved = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == ".": continue
+
+                amount = 0
+                for k in range(-1, 2):
+                    if i == 0 and k == -1: continue
+                    for l in range(-1, 2): 
+                        if  j == 0 and l == -1: continue
+                        try: 
+                            if k == l == 0: continue
+                            if grid[i + k][j + l] == "@": amount += 1
+                        except IndexError:
+                            continue
+
+                if amount < 4: 
+                    loopRemoved += 1
+                    grid[i][j] = "."
+                    total += 1
+
+    return total
+
+
 # with open("day4/testInput.txt") as f:
 with open("day4/input.txt") as f:
-    lines = f.readlines()
+    lines = [[i for i in line.strip()] for line in f.readlines()]
+
+print(findValid(lines))
